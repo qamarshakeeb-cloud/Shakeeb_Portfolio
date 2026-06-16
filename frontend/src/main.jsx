c8e26fe492db56744912f18c346b7486727e4f85
MSQ104 <qamarshakeeb@gmail.com>
2026-06-16 21:05:34 +0200

    changed git link

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ const profile = {
     email: 'mdshakeebqamar@gmail.com',
     location: 'Bremen, Germany',
     linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    github: 'https://github.com/qamarshakeeb-cloud/Shakeeb_Portfolio',
     resume: '/resume.pdf',
 };
 
